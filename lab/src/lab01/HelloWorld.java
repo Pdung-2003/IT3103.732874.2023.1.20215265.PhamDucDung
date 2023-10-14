fac1da77e8b31d41d6f9728c6c42076bc138d8f1
dungpd2003 <dung.pd215265@sis.hust.edu.vn>
2023-10-14 16:49:54 +0700

    Chu thich

---
+++ lab/src/lab01/HelloWorld.java
@@ -0,0 +1,10 @@
+package lab01;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n Cac ban");
+        System.out.println("Hello \t world! ");
+    }
+}
