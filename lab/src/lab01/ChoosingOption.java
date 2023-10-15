0ed2f137f5cc3df26d0b8260b70da61d9968e1b5
dungpd2003 <dung.pd215265@sis.hust.edu.vn>
2023-10-15 19:55:09 +0700

    Lab01

---
+++ lab/src/lab01/ChoosingOption.java
@@ -0,0 +1,27 @@
+package lab01;
+
+import javax.swing.*;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        Object[] options = {"Yes", "No"};
+        int option = JOptionPane.showOptionDialog(null,
+                "Do you want to change to the first class tickets?",
+                "Confirmation",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        if (option == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "You've chosen: Yes");
+        } else if (option == JOptionPane.NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "You've chosen: No");
+        } else {
+            JOptionPane.showMessageDialog(null, "Dialog closed without selection");
+        }
+
+        System.exit(0);
+    }
+}
