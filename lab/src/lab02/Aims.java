02229775577628498c8bf4f2dc0afce31e4014c6
dungpd2003 <dung.pd215265@sis.hust.edu.vn>
2023-11-15 21:40:20 +0700

    nhanh

--- lab/src/lab02/Aims.java
+++ lab/src/lab02/Aims.java
@@ -20,5 +20,5 @@ public class Aims {
         System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
     }
-    // class member princart search topic store
+    // class member princart search topic store packages
 }
